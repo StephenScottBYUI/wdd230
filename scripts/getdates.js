a479aafb470a90f48390d22af90a596184f3a427
StephenScottBYUI <saukalt41@gmail.com>
2025-01-22 21:54:32 -0500

    added a map and buttons still need to finish

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -9,3 +9,11 @@ document.getElementById('currentYear').textContent = getCurrentYear();
 
 // Update the last modified date in the footer
 document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
