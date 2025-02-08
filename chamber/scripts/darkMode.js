fb691387fea2a1c51d68e390928ce6eb111e9ed2
StephenScottBYUI <saukalt41@gmail.com>
2025-02-08 17:50:49 -0500

    added localStorage for the preferences.

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -3,15 +3,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const themeStylesheet = document.getElementById('theme-stylesheet');
     const logo = document.querySelector('.logo')
 
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        switchInput.check = true;
+        themeStylesheet.setAttribute('href', 'styles/dm-colors-and-fonts.css');
+        logo.setAttribute('src', 'images/dm-willingboro-logo.svg')
+    }
+
     switchInput.addEventListener('change', function () {
         if (switchInput.checked) {
 
             themeStylesheet.setAttribute('href', 'styles/dm-colors-and-fonts.css');
             logo.setAttribute('src', 'images/dm-willingboro-logo.svg')
+            localStorage.setItem('darkMode', 'enabled');
         } else {
 
             themeStylesheet.setAttribute('href', 'styles/colors-and-fonts.css');
             logo.setAttribute('src', 'images/willingboro-logo.svg')
+            localStorage.setItem('darkMode', 'disabled');
         }
     });
 });
