696f4aaf104f0138195d17404fda2476a281655a
StephenScottBYUI <saukalt41@gmail.com>
2025-02-16 13:03:55 -0500

    changed a bit so that it makes sense if there is no wind chill

--- chamber/scripts/windChill.js
+++ chamber/scripts/windChill.js
@@ -7,7 +7,7 @@ function calcWindChill() {
     }
     else {
         const windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wind, 0.16) + 0.4275 * temp * Math.pow(wind, 0.16);
-        document.querySelector('#windChill').textContent = `${Math.round(windChill)}`;
+        document.querySelector('#windChill').textContent = `${Math.round(windChill)}° F`;
     }
 }
 
