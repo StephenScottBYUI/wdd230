4a27f2d87a264bf9840304b0b4cc94eaa5ebbc91
StephenScottBYUI <saukalt41@gmail.com>
2025-02-15 22:44:39 -0500

    added wind chill

---
+++ chamber/scripts/windChill.js
@@ -0,0 +1,17 @@
+function calcWindChill() {
+    const temp = parseFloat(document.querySelector('#tempNumber').textContent);
+    const wind = parseFloat(document.querySelector('#windNumber').textContent);
+
+    if (temp >= 50 || wind <= 3) {
+        document.querySelector('#windChill').textContent = "N/A";
+    }
+    else {
+        const windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(wind, 0.16) + 0.4275 * temp * Math.pow(wind, 0.16);
+        document.querySelector('#windChill').textContent = `${Math.round(windChill)}`;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    calcWindChill();
+});
+
