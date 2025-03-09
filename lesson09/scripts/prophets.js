610a18ab638d7e5a5aaad6183a96d0df6dc85c49
StephenScottBYUI <saukalt41@gmail.com>
2025-03-08 21:25:06 -0500

    created a JSON list

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -4,8 +4,36 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets);
+    displayProphets(data.prophets);
 
 }
 
 getProphetData();
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let birth = document.createElement('p');
+        let birthPlace = document.createElement('p');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birth.textContent = `Date of Birth: ${prophet.birthdate}`
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `This is an image of ${prophet.name} ${prophet.lastname}`)
+        portrait.setAttribute('loading', 'lazy')
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+        card.setAttribute('class', 'item')
+
+        card.appendChild(fullName);
+        card.appendChild(birth);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+
+    });
+}
--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,47 @@
 const baseURL = "https://stephenscottbyui.github.io/wdd230/";
 const linksURL = "https://stephenscottbyui.github.io/wdd230/data/links.json";
+const fullList = document.getElementById('linkList');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data.lessons);
 
 }
 
 getLinks();
+
+const displayLinks = (lessons) => {
+    lessons.forEach((lesson) => {
+        let listItem = document.createElement('li');
+        let lessonSpan = document.createElement('span');
+
+        lessonSpan.textContent = `Lesson ${lesson.lesson}: `;
+        lessonSpan.style.fontWeight = "bold";
+
+
+        listItem.appendChild(lessonSpan);
+
+        let listContent = lessonLinks(lesson.links);
+
+        fullList.appendChild(listItem);
+        listItem.appendChild(listContent);
+
+    })
+}
+
+const lessonLinks = (links) => {
+    let listContainer = document.createElement('span');
+
+    links.forEach((link) => {
+
+        let lessonLink = document.createElement('a');
+
+        lessonLink.setAttribute('href', link.url);
+        lessonLink.textContent = `${link.title}`;
+        listContainer.setAttribute('class', 'list-container');
+        listContainer.appendChild(lessonLink);
+    });
+
+    return listContainer;
+}
